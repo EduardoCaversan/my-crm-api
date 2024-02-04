ddade8947e5f4b1e911e18ecbdaa09ae4bc2a5ad
Eduardo Caversan <eduardo@adven.tech>
2024-02-04 20:37:23 -0300

    #added-environment-and-login-1

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,9 @@ const userController = require('../controllers/userController');
 
 router.get('/users/listAllUsers', userController.getAllUsers);
 router.post('/users/createUser', userController.createUser);
+router.post('/users/loginUser', userController.loginUser);
 router.delete('/users/:userId', userController.deleteUser);
+router.get('/users/:userId/consumers', userController.listConsumers);
 router.post('/users/:userId/consumers', userController.addConsumer);
 router.put('/users/:userId/consumers/:consumerId', userController.editConsumer);
 router.delete('/users/:userId/consumers/:consumerId', userController.removeConsumer);
